d098b681ecfe791abf3988b5247de677c1da665b
Abel Perez <abelper54@gmail.com>
2022-05-24 08:45:51 -0400

    readHTML implementation

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import Epitelete from "Epitelete";
+import perf2html from "./perf2html"
+
+class EpiteletePerfHtml extends Epitelete{
+    constructor(pk, docSetId) {
+        super(pk, docSetId);
+    }
+    async readHTML(bookCode) {
+        const perf = await super.readPerf(bookCode);
+        const docSetId = this.docSetId;
+        const doc = perf.docSets[docSetId].documents[bookCode];
+
+        const ret = {
+            docSetId,
+            mainSequenceId: doc.mainSequence,
+            headers: doc.headers,
+            sequenceHtml: {},
+        };
+        Object.keys(doc.sequences)
+            .forEach(seqId => { ret.sequenceHtml[seqId] = perf2html(doc, seqId) });
+        
+        return JSON.stringify(ret, null, 2);
+    }
+}
+
+export default EpiteletePerfHtml;
