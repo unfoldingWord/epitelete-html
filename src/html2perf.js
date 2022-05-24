f62ef8c26adfeaf661de4a64ff7b55970457bf91
Abel Perez <abelper54@gmail.com>
2022-05-24 15:07:13 -0400

    fx indentation

--- src/html2perf.js
+++ src/html2perf.js
@@ -1,33 +1,31 @@
 import { parse } from 'node-html-parser';
 
 function html2perf(html) {
-  const json = JSON.parse(html);
-  // console.log({ html: json.sequenceHtml[json.mainSequenceId] });
-  const mainSequenceHtml = parse(json.sequenceHtml[json.mainSequenceId]);
-  const mainSequenceElement = mainSequenceHtml.firstChild;
-  const blocksContainer = mainSequenceElement.querySelector('.block, .graft').parentNode;
+    const json = JSON.parse(html);
+    const mainSequenceHtml = parse(json.sequenceHtml[json.mainSequenceId]);
+    const mainSequenceElement = mainSequenceHtml.firstChild;
+    const blocksContainer = mainSequenceElement.querySelector('.block, .graft').parentNode;
+    
+    const blocks = blocksContainer.childNodes.reduce((prev, curr, idx) => {
+        if (curr.nodeType !== 1) return prev;
+        
+        const type = curr.getAttribute("data-type");
+        const subType = type + 'Type';
+        
+        const block = {
+            "type": type,
+            "subType": subType,
+        }
+        prev.push(block);
+        
+        return prev;
+    }, []);
 
-  const blocks = blocksContainer.childNodes.reduce((prev, curr, idx) => {
-
-    if (curr.nodeType !== 1) return prev;
-
-    const type = curr.getAttribute("data-type");
-    const subType = type + 'Type';
-
-    const block = {
-      "type": type,
-      "subType": subType,
+    const perf = {
+        blocks,
+        type: mainSequenceElement.getAttribute("data-sequenceType"),
+        selected: true,
     }
-    prev.push(block);
-
-    return prev;
-  }, []);
-
-  const perf = {
-    blocks,
-    type: mainSequenceElement.getAttribute("data-sequenceType"),
-    selected: true,
-  }
-  return perf;
+    return perf;
 }
 export default html2perf;
