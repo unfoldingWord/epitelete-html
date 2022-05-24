7e559f47e169bae00154841fb67ea75218a56bb0
Abel Perez <abelper54@gmail.com>
2022-05-24 13:25:26 -0400

    added type to perf

--- src/html2perf.js
+++ src/html2perf.js
@@ -3,8 +3,10 @@ import { parse } from 'node-html-parser';
 function html2perf(html) {
   const json = JSON.parse(html);
   const mainSequenceHtml = parse(json.sequenceHtml[json.mainSequenceId]);
+  const mainSequenceElement = mainSequenceHtml.firstChild;
   const perf = {
     blocks: [],
+    type: mainSequenceElement.getAttribute("data-sequenceType"),
     mainSequenceHtml
   }
   return perf;
