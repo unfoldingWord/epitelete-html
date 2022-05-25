570c812a746ae9e00c48c72d5f4cb5adc08fabc4
Mark Howe <mvahowe@gmail.com>
2022-05-25 10:34:02 +0200

    Split up tests

--- test/code/writeHtml.cjs
+++ test/code/writeHtml.cjs
@@ -4,7 +4,7 @@ const fse = require("fs-extra");
 const {UWProskomma} = require("uw-proskomma");
 const EpiteletePerfHtml = require("../../src/index").default;
 
-const testGroup = "Smoke";
+const testGroup = "writeHtml";
 
 const pk = new UWProskomma();
 // const succinctJson = fse.readJsonSync(path.resolve(path.join(__dirname, "..", "test_data", "fra_lsg_succinct.json")));
@@ -12,16 +12,15 @@ const succinctJson = fse.readJsonSync(path.resolve(path.join(__dirname, "..", "t
 pk.loadSuccinctDocSet(succinctJson);
 
 test(
-    `Instantiate Class (${testGroup})`,
+    `writeHTML doesn't die (${testGroup})`,
     async function (t) {
         try {
-            t.plan(5)
-            t.throws(() => new EpiteletePerfHtml(pk), "2 arguments");
-            t.throws(() => new EpiteletePerfHtml(pk, "eBible/fra_fraLSG"), "docSetId is not present");
+            t.plan(2);
             const instance = new EpiteletePerfHtml(pk, "DBL/eng_engWEBBE");
-            t.ok(instance);
-            t.ok(instance.pk);
-            t.ok(instance.docSetId);
+            const bookCode = "LUK"
+            const html = await instance.readHTML(bookCode);
+            t.ok(html);
+            t.doesNotThrow(() => instance.writeHTML(html));
         } catch (err) {
             console.log(err);
         }
