54f5143ba4557433953839210a7072616b7620b1
Abel Perez <abelper54@gmail.com>
2022-07-12 12:17:55 -0400

    update generatePerfHtml

--- utils/generatePerfHtml.js
+++ utils/generatePerfHtml.js
@@ -10,8 +10,6 @@ const pk = new UWProskomma();
 console.log("Loading source to proskomma...");
 const succinctJson = fse.readJsonSync(path.resolve(path.join(__dirname, "..", "test", "test_data", "fra_lsg_succinct.json")));
 const docSetId = "eBible/fra_fraLSG";
-// const succinctJson = fse.readJsonSync(path.resolve(path.join(__dirname, "..", "test", "test_data", "eng_engWEBBE_succinct.json")));
-// const docSetId = "DBL/eng_engWEBBE";
 pk.loadSuccinctDocSet(succinctJson);
 
 export const generatePerfHtml = async () => {
@@ -23,15 +21,21 @@ export const generatePerfHtml = async () => {
     console.log(`Generating perfHtml file...`);
   
   const perfHtml = await instance.readHtml(bookCode);
-  const sequenceId = args[1] || perfHtml.mainSequenceId;
+  const sequenceId = args[1] === "main" ? perfHtml.mainSequenceId : args[1];
+
+  const output = {
+    content: sequenceId ? beautify(perfHtml.sequencesHtml[sequenceId]) : JSON.stringify(perfHtml, undefined, 2),
+    ext: sequenceId ? "html" : "json"
+  };
+
   const dir = path.resolve(__dirname, "..", "test", "test_data", "generated");
   if (! fse.pathExistsSync(dir)) 
     fse.mkdirSync(dir);
   
   const safeDocSetId = instance.docSetId.replace("/", "-");
-  const fileName = `${bookCode}-${safeDocSetId}-perf_v${perfHtml.schema.structure_version}.html`;
+  const fileName = `${bookCode}-${safeDocSetId}-${sequenceId && `${sequenceId}-`}perfhtml_v${perfHtml.schema.structure_version}.${output.ext}`;
   const filePath = path.resolve(dir, fileName);
-  fse.writeFileSync(filePath, beautify(perfHtml.sequencesHtml[sequenceId]), {encoding: 'utf8'});
+  fse.writeFileSync(filePath, output.content, {encoding: 'utf8'});
   console.log("\u001B[32m", `✔ HTML file generated for ${bookCode} at ${filePath}`, "\u001B[0m");
 };
 
